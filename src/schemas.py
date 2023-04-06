d1753bd28c67a1990aa1bf3c1f7ed8519893ad52
Felipe <felipesbs@al.insper.edu.br>
2023-04-06 18:18:36 -0300

    Modifica schemas

--- src/schemas.py
+++ src/schemas.py
@@ -6,11 +6,14 @@ class Filme(BaseModel):
     """Objeto Filme"""
 
     id_filme: Optional[int] = Field(
-        title="ID único do filme na base de dados. Propriedade automática.",
+        title="ID único do filme na base de dados.",
+        description="Propriedade automática. Inteiro positivo.",
+        ge = 0,
     )
     titulo: str = Field(
         title="Título do filme.",
         max_length=100,
+        example = "O Poderoso Chefão",
     )
 
 
@@ -18,15 +21,27 @@ class Avaliacao(BaseModel):
     """Avaliação de um filme feita por um usuário"""
 
     id_avaliacao: Optional[int] = Field(
-        title="ID único da avaliação na base de dados. Propriedade automática.",
+        title="ID único da avaliação na base de dados.",
+        description="Propriedade automática. Inteiro positivo.",
+        ge = 0,
+
     )
     id_filme: int = Field(
         title="Identificador do filme avaliado.",
+        description="Inteiro positivo.",
+        ge = 0,
     )
     nota: float = Field(
-        title="Nota, de 0 a 10, para o filme.",
+        title="Nota atribuída ao filme.",
+        description="Número real entre 0 e 10.",
+        ge = 0,
+        le = 10,
+        example = 8.5,
+
     )
     comentario: str = Field(
-        title="Crítica elaborada sobre o filme.",
+        title="Comentario elaborado sobre o filme.",
+        description="Texto livre com até 400 caracteres.",
         max_length=400,
+        example = "Um clássico do cinema.",
     )
